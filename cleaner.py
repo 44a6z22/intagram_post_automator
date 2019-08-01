1fc05bad0dfaac509725a178556fde355965cb62
hamdaoui hamza <hhamdaoui31@gmail.com>
2019-08-01 20:19:26 +0100

    coulpe of terminal commands to help setting up things

--- cleaner.py
+++ cleaner.py
@@ -13,20 +13,34 @@ def clean(jsonFile):
         json.dump(data, f, indent=4)
         print("Done")
 
-def reset(jsonFile):
-    with open(jsonFile, 'r') as f: 
-        cache = json.load(f)
 
-    cache["images_path"] = None
-    cache['username'] = None
-    cache['password'] = None
-    cache['isFirstTime'] = True
-    
-    with open(jsonFile,"w") as target:
-        json.dump(cache, target, indent=4 )
-    
-    return print("cache reset") 
 
 if __name__ == "__main__":
-    if sys.argv[2] == "--reset" and sys.argv[1] == "cache":
-        reset("json_data/cashe.json")
+    
+    jsonFile = "json_data/cashe.json"
+
+    with open(jsonFile, 'r') as f:
+        cache = json.load(f)
+
+    if sys.argv[1] == "--reset-cache":
+        cache["images_path"] = None
+        cache['username'] = None
+        cache['password'] = None
+        cache['isFirstTime'] = True
+
+    elif sys.argv[1] == "--set-username":
+        cache["username"] = sys.argv[2] 
+    elif sys.argv[1] == "--set-password":
+        cache["password"] = sys.argv[2]
+    elif sys.argv[1] == "--set-path":
+        cache["images_path"] = sys.argv[2] 
+    elif sys.argv[1] == "--help": 
+        print("--")
+
+    if cache["username"] != None and cache["password"] != None and cache["images_path"] != None : 
+        cache["isFistTime"] = False 
+   
+    with open(jsonFile, "w") as target:
+        json.dump(cache, target, indent=4)
+        print('Done')
+        
