47f69d64fe7737f12312d12b3ffb1c739a0438f6
hamdaoui hamza <hhamdaoui31@gmail.com>
2019-08-01 21:49:59 +0100

    okay

--- cleaner.py
+++ cleaner.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
 
         if sys.argv[1] == "--reset-datastore":
-            datastore = Cleaner("json_data/data.json")
+            datastore = Cleaner.Cleaner("json_data/data.json")
             datastore.resetData()
 
         obj = Cleaner.Cleaner(jsonFile)
