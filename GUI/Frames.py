fabce2a0dab21cf2a2d6e26ba0a1122e14f718cc
hamdaoui hamza <hhamdaoui31@gmail.com>
2019-08-17 05:46:50 +0100

    login info GUI done

--- GUI/Frames.py
+++ GUI/Frames.py
@@ -1,54 +1,87 @@
 #!/usr/local/bin/python
 from Tkinter import * 
 import json
+import time
 
 
-class loginButtons(Frame): 
+class Menus(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.grid()
-        self.createButtons()
+        self.grid(row=0)
+        self.setMenu()
 
-    def createButtons(self):
-        self.quitButton = Button(self, text="Save", command=self.save)
-        self.quitButton.grid(row=0, column=0, ipadx=50)
-        self.quitButton = Button(self, text="Quit", command=self.quit)
-        self.quitButton.grid(row=0, column=1, ipadx=50)
+    def setMenu(self): 
+        # how to make a sub menu layout
+        # menu = Menu(self.master)
+        # self.master.config(menu=menu)
+        # subMenu = Menu(menu)
+        # menu.add_cascade(label="account", menu=subMenu)
+        # subMenu.add_command(label="login", command=self.quit)
 
-    def save(self): 
-
-        with open("../json_data/dataa.json", 'r') as c:
-            cache = json.load(c)
-
-        cache["ok"][len(cache["ok"]) -1] = "ssssok"
-
-        with open("../json_data/dataa.json", 'w') as c:
-            json.dump(cache, c, indent=4)
-
+        # how to make a simple menu layout
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+        menu.add_command(label="Quit", command=self.quit)
 
 class loginEntries(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.grid(row=0)
+        self.grid(row=1, ipady=20, ipadx=20)
         self.createLabels()
         self.createInputs()
 
     def createLabels(self):
         self.userNameLable = Label(self.master, text="username : ")
         self.passwordLabel = Label(self.master, text="password : ")
-        self.userNameLable.grid(row=0, column=0, sticky=E)
-        self.passwordLabel.grid(row=1, column=0, sticky=E)
+        self.pathLabel = Label(self.master, text="folder's path : ")
 
-    def createInputs(self): 
+        self.userNameLable.grid(row=0, sticky=E, ipadx= 20)
+        self.passwordLabel.grid(row=1, sticky=E, ipadx=20)
+        self.pathLabel.grid(row=3, sticky=E, ipadx=20)
+
+    def createInputs(self):
         self.userNameEntry = Entry(self.master)
         self.passwordEntry = Entry(self.master)
-        self.userNameEntry.grid(row=0, column=1)
-        self.passwordEntry.grid(row=1, column=1)
+        self.pathEntry = Entry(self.master)
+        self.userNameEntry.grid(row=0, column=1, ipadx=20)
+        self.passwordEntry.grid(row=1, column=1, ipadx=20)
+        self.pathEntry.grid(row=3, column=1, ipadx=20)
 
 
-class SaveInfoButtons(Frame): 
+
+class FilesInfoFrame(Frame):
     pass
 
 
-class FilesInfoFrame(Frame): 
+class loginButtons(Frame): 
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.grid(row=4, columnspan=2, ipady=10, sticky=S)
+        self.createButtons()
+
+    def createButtons(self):
+        self.quitButton = Button(self, text="Login", command=self.save)
+        self.quitButton.grid(row=4, column=0, ipadx=40)
+        self.quitButton = Button(self, text="Quit", command=self.quit)
+        self.quitButton.grid(row=4, column=1, ipadx=40)
+
+    def save(self): 
+
+        with open("../json_data/dataa.json", 'r') as c:
+            cache = json.load(c)
+
+        cache["ok"][0] = "ssssok"
+
+        with open("../json_data/dataa.json", 'w') as c:
+            json.dump(cache, c, indent=4)
+
+        label = Label(self.master, text ="Done")
+        label.grid(row=4, columnspan=2)
+        time.sleep(2)
+        self.quit()
+
+
+
+
+class SaveInfoButtons(Frame):
     pass
--- GUI/login.py
+++ GUI/login.py
@@ -6,7 +6,10 @@ from Frames import *
 if __name__ == "__main__":
     
     root = Tk()
+    root.title("InstaPy Login GUI")
 
+    
+    menu = Menus(root)
     inputs = loginEntries(root)
     loginButtons = loginButtons(root)
 
